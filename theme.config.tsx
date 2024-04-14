a0a4460dc7ae72346e2538e7d6c837e2faeec56c
Matisse <effxct.mp@gmail.com>
2024-04-14 17:23:06 +0200

    Test

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>NSFW World #18+ Documentation</span>,
+  logo: <><span>NSFW World #18+ Documentation</span></>,
   project: {
     link: 'https://github.com/shuding/nextra-docs-template',
   },
